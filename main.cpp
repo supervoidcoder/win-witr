b94b9d2a5dcdba8e062a9d4f931db3a5c8b7c40d
supervoidcoder <88671013+supervoidcoder@users.noreply.github.com>
2026-01-23 08:45:44 -0500

    fix: i keep forgetting to add semicolons

--- main.cpp
+++ main.cpp
@@ -265,7 +265,7 @@ UPDATE: This is done now!!
     // tells us that our target pid is it's parent. This time, we don't have to worry about
     // Checking if the parent is alive, because, well, since the target IS the parent, 
     // it must be alive.
-    int children = 0 // i wonder what would happen if you could set an emoji as var name
+    int children = 0; // i wonder what would happen if you could set an emoji as var name
     if (Process32First(hSnapshot, &pe32)) {
         do {
             
@@ -282,9 +282,9 @@ UPDATE: This is done now!!
                     // you might've noticed this doesn't have an emplace_front() like emplace_back() since 
                     // it's inefficient and the creators of the vector lib didn't do it
                     pidNames.emplace(pidNames.begin(), pe32.th32ProcessID);
-                    ULONGLONG childTime == GetProcessCreationTime(pe32.th32ProcessID)
-                    exeTimes.emplace(exeTimes.begin(), childTime) // we don't even use this but we need to keep all the vectors the same length
-                    parentPids.emplace(parentPids.begin(), pe32.th32ProcessID) // just fill it up, we aren't using it
+                    ULONGLONG childTime == GetProcessCreationTime(pe32.th32ProcessID);
+                    exeTimes.emplace(exeTimes.begin(), childTime); // we don't even use this but we need to keep all the vectors the same length
+                    parentPids.emplace(parentPids.begin(), pe32.th32ProcessID); // just fill it up, we aren't using it
                     children++; // keeps track of how many children we have (that sounds wrong when you say it)
 
                 }
