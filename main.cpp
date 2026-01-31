3c3df854a009e13d551479362255b7a822eef756
supervoidcoder <88671013+supervoidcoder@users.noreply.github.com>
2026-01-30 23:12:37 -0500

    fix: try adding #define

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (C) 2025 supervoidcoder
 // This file is part of win-witr.
- 
+
+#define _WIN32_WINNT  0x0501
+#define NO_STRICT
 #include <windows.h>
 #include <winternl.h>
 #include <tlhelp32.h> 
