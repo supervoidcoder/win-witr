fc16ada8a5be7776757780fdbb418e6e0c897d87
supervoidcoder <88671013+supervoidcoder@users.noreply.github.com>
2026-02-13 12:22:06 -0500

    fix:

--- main.cpp
+++ main.cpp
@@ -2103,35 +2103,7 @@ void PIDinspect(const std::vector<DWORD>& pids, const std::vector<std::string>& 
             pidMap.emplace(pe32.th32ProcessID, pe32);
         } while (Process32Next(hshot, &pe32));
     }
-	std::string procName = GetProcessNameFromPid(pid, hshot);
-	std::string process = GetWindowTitle(hshot);
 	
-	
-	if (virtualTerminalEnabled) {
-		if (!process.empty()) { 
-		std::cout << "\033[34mTarget:\033[0m " << procName << "\033[0m" << std::endl;
-		std::cout << "\033[34mProcess:\033[0m " << process << "\033[90m (pid " << std::to_string(pid) << ")\033[0m" << std::endl;
-		} else 
-		if (procName == "") { 
-			std::cout << "\033[34mTarget:\033[0m N/A\n\033[34mProcess:\033[0m N/A\n";
-		} else {
-		std::cout << "\033[34mTarget:\033[0m " << procName << "\033[0m" << std::endl;
-		std::cout << "\033[34mProcess:\033[0m " << procName << "\033[90m (pid " << std::to_string(pid) << ")\033[0m" << std::endl;
-		}
-	} else {
-			if (!process.empty()) { 
-		std::cout << "Target: " << procName << std::endl;
-		std::cout << "Process: " << process << " (pid " << std::to_string(pid) << ")" << std::endl;
-		} else {
-		if (procName == ""){
-			std::cout << "Target: N/A\nProcess: N/A\n";
-				} else {
-		std::cout << "Target: " << procName << std::endl;
-		std::cout << "Process: " << procName << " (pid " << std::to_string(pid) << ")" << std::endl;
-		}
-		
-	}
-	}
 	
 
 	
@@ -2174,6 +2146,35 @@ void PIDinspect(const std::vector<DWORD>& pids, const std::vector<std::string>& 
         
         
     }
+	std::string procName = GetProcessNameFromPid(pid, hshot);
+	std::string process = GetWindowTitle(hProcess);
+	
+	
+	if (virtualTerminalEnabled) {
+		if (!process.empty()) { 
+		std::cout << "\033[34mTarget:\033[0m " << procName << "\033[0m" << std::endl;
+		std::cout << "\033[34mProcess:\033[0m " << process << "\033[90m (pid " << std::to_string(pid) << ")\033[0m" << std::endl;
+		} else 
+		if (procName == "") { 
+			std::cout << "\033[34mTarget:\033[0m N/A\n\033[34mProcess:\033[0m N/A\n";
+		} else {
+		std::cout << "\033[34mTarget:\033[0m " << procName << "\033[0m" << std::endl;
+		std::cout << "\033[34mProcess:\033[0m " << procName << "\033[90m (pid " << std::to_string(pid) << ")\033[0m" << std::endl;
+		}
+	} else {
+			if (!process.empty()) { 
+		std::cout << "Target: " << procName << std::endl;
+		std::cout << "Process: " << process << " (pid " << std::to_string(pid) << ")" << std::endl;
+		} else {
+		if (procName == ""){
+			std::cout << "Target: N/A\nProcess: N/A\n";
+				} else {
+		std::cout << "Target: " << procName << std::endl;
+		std::cout << "Process: " << procName << " (pid " << std::to_string(pid) << ")" << std::endl;
+		}
+		
+	}
+	}
 
      
     char exePath[MAX_PATH] = {0};
